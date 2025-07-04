10a02668db616ec4fd6f73380c19e5fd02c9e5fe
Chrani Jahri <145993450+TJahari@users.noreply.github.com>
2025-07-04 14:24:26 +0200

    Update wrongData.js

--- M165/src/wrongData.js
+++ M165/src/wrongData.js
@@ -1,19 +1,17 @@
 db.mietvertrag.insertMany([
 {
-  "_id": "not-an-object",  // ❌ should be an object with $oid
-  "von": { "$date": 123456 },  // ❌ $date should be a string
-  "bis": {},  // ❌ missing $date
+  "_id": "not-an-object",
+  "von": { "$date": 123456 },
+  "bis": {},
   "auto": {
     "marke": "Toyota",
     "modell": "Corolla"
-    // ❌ missing 'kennzeichen' and 'standort'
   },
   "mieter": {
-    "id_": { "$oid": 12345 },  // ❌ $oid should be a string
+    "id_": { "$oid": 12345 },
     "name": "Max Mustermann"
-    // ❌ missing 'ausweisnr' and 'adresse'
   },
-  "preis": "free",  // ❌ should be a number
-  "standortname": 42  // ❌ should be a string
+  "preis": "free",
+  "standortname": 42
 }
 ]);
